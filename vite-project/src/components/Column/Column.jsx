697b2490dab24ea0bdca9f0a81607ac26a82df5f
Yulenka5 <yulia1995m@mail.ru>
2024-05-16 23:15:53 +0500

    проверка

---
+++ vite-project/src/components/Column/Column.jsx
@@ -0,0 +1,49 @@
+import Card from "../Card/Card";
+
+function Column() {
+  return (
+    <>
+      <div className="main__column column">
+        <div className="column__title">
+          <p>Без статуса</p>
+        </div>
+        <div className="cards">
+          <Card />
+        </div>
+      </div>
+      <div className="main__column">
+        <div className="column__title">
+          <p>Нужно сделать</p>
+        </div>
+        <div className="cards">
+          <Card />
+        </div>
+      </div>
+      <div className="main__column">
+        <div className="column__title">
+          <p>В работе</p>
+        </div>
+        <div className="cards">
+          <Card />
+        </div>
+      </div>
+      <div className="main__column">
+        <div className="column__title">
+          <p>Тестирование</p>
+        </div>
+        <div className="cards">
+          <Card />
+        </div>
+      </div>
+      <div className="main__column">
+        <div className="column__title">
+          <p>Готово</p>
+        </div>
+        <div className="cards">
+          <Card />
+        </div>
+      </div>
+    </>
+  );
+}
+export default Column;
--- vite-project/src/components/WelcomeMessage.jsx
+++
@@ -1,5 +0,0 @@
-function WelcomeMessage() {
-    return <h1>Добро пожаловать на наш сайт!</h1>
-}
-
-export default WelcomeMessage
